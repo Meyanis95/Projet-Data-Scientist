dab68d95cfc3b6ec6476826abfa4205fed6463ad
Meyanis95 <b00694485@essec.edu>
2020-07-09 17:15:44 +0200

    1er commit

---
+++ lib/00_journalists.rb
@@ -0,0 +1,134 @@
+#--------------------------------------------------------------------------------
+#Hello !!
+#Tu peux directement exécuter le programme pour voir mes réponses aux questions ! 
+#--------------------------------------------------------------------------------
+
+data = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+def how_many_handle(data)
+    puts data.length
+end 
+
+def short_handle(data)
+    min = []
+    data.each do |i|
+        min.append(i.length)
+    end
+    data.each do |i|
+        if i.length == min.min
+            puts i
+        end 
+    end
+end 
+
+def five_car(data)
+    five = []
+    data.each do |i|
+        if (i.length - 1) == 5
+            five.append(i)
+        end
+    end
+    puts five.length
+end
+
+def maj(data)
+    maj = []
+    data.each do |i|
+        if i.slice(1).capitalize == i.slice(1)
+            maj.append(i)
+        end 
+    end 
+    puts maj.length
+end
+
+def sort(data)
+    puts data.sort
+end
+
+def sort_size(data)
+    puts data.sort_by {|element| element.length}
+end
+
+def penser(data)
+    puts data.index("@epenser")
+end
+
+def repartition(data)
+    distrib = {}
+    20.times do |i|
+        distrib[i] = 0
+    end
+
+    distrib.each do |k,v|
+        data.each do |i|
+            if (i.length - 1) == k
+            distrib[k] += 1
+            end 
+        end
+    end
+    puts distrib
+end
+
+def perform(data)
+    data = data
+    puts "Hello, bienvenue son mon programme !"
+    puts "Pour passer à la première question, tape entrer"
+    gets.chomp
+    puts "Combien y a-t-il de handle dans cette array ?"
+    print "> "
+    #Question 1
+    how_many_handle(data)
+    #Question 1
+    puts "Tape entrer pour aller à la question suivante !"
+    gets.chomp 
+    puts "Quelle est le handle le plus court de cette liste ?"
+    print "> "
+    #Question 2
+    short_handle(data)
+    #Question 2
+    puts "Tape entrer pour aller à la question suivante !"
+    gets.chomp 
+    puts "Combien y-a-t'il de handle contenant 5 caractères ?"
+    print "> "
+    #Question 3
+    five_car(data)
+    #Question 3
+    puts "Tape entrer pour aller à la question suivante !"
+    gets.chomp 
+    puts "Combien commencent par une majuscule ?"
+    print "> "
+    #Question 4
+    maj(data)
+    #Question 4
+    puts "Tape entrer pour aller à la question suivante !"
+    gets.chomp 
+    puts "Trie la liste de handle par ordre alphabétique"
+    print "> "
+    #Question 5 
+    sort(data)
+    #Question 5
+    puts "Tape entrer pour aller à la question suivante !"
+    gets.chomp 
+    puts "Trie la liste de handle par taille des handle"
+    print "> "
+    #Question 6
+    sort_size(data)
+    #Question 6
+    puts "Tape entrer pour aller à la question suivante !"
+    gets.chomp 
+    puts "Quelle est la position dans l'array de la personne @epenser ?"
+    print "> "
+    #Question 7 
+    penser(data)
+    #Question 7
+    puts "Tape entrer pour aller à la question suivante !"
+    gets.chomp 
+    puts "Sors-moi une répartition des handle par taille de ces derniers ?"
+    print "> "
+    #Question 8
+    repartition(data)
+    puts "Merci à toi :) !"
+    puts "J'espère que c'était clair !"
+end
+
+perform(data)
